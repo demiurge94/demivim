22771c1d5c43f3ef3d69d9bb1dce1ea217291a68
Erick Vicencio Del Angel <erickvicencio@gmail.com>
2024-01-19 22:58:29 -0800

    add set

--- lua/eridemi/init.lua
+++ lua/eridemi/init.lua
@@ -1,5 +1,5 @@
 require("eridemi.remap")
-print("Hello from eridemi")
+require("eridemi.set")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
