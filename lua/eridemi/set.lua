776fd30461fc31e9455a466adc9c441dbe7c7a03
erick vicencio <erickvicencio@gmail.com>
2024-01-20 17:06:02 -0800

    some keymaps

--- lua/eridemi/set.lua
+++ lua/eridemi/set.lua
@@ -13,7 +13,22 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir" 
 
+vim.opt.hlsearch = false 
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
 
 
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "90"
+
+vim.g.mapleader = " "
 
 
